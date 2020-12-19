e583f9b59527350856e954ca6e44334ee7c93df9
Akshay N A <akshayna04698@gmail.com>
2020-12-19 12:20:58 +0530

    Added Db Context for migration

---
+++ EcommMVC/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace EcommMVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+
+        }
+    }
+}
